bcd0229460c44686a0fd11e3db9d3809f1b6ee7d
vadde suresh <sureshvadde1076@gmail.com>
2023-01-05 21:30:57 +0530

    done

--- src/main/java/com/driver/BookController.java
+++ src/main/java/com/driver/BookController.java
@@ -3,10 +3,9 @@ package com.driver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("books")
@@ -16,8 +15,39 @@ public class BookController {
 
     // One example controller, make the rest by yourself
     @PostMapping("/create-book")
-    public ResponseEntity createBook(@RequestBody Book book){
+    public ResponseEntity<Book> createBook(@RequestBody Book book){
         Book newbook = bookService.createBook(book);
         return new ResponseEntity<>(newbook, HttpStatus.CREATED);
     }
+    @GetMapping("get-book-by-id/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable int id){
+        Book book = bookService.findBookById(String.valueOf(id));
+        return new ResponseEntity<>(book,HttpStatus.CREATED);
+    }
+    @GetMapping("get-all-books")
+    public ResponseEntity<List<Book>> GetAllBooks(){
+        List<Book> books = bookService.findAllBooks();
+        return new ResponseEntity<>(books,HttpStatus.CREATED);
+    }
+    @GetMapping("get-books-by-author")
+    public ResponseEntity<List<Book>> getBooksByAuthor(@RequestParam String author){
+        List<Book> books = bookService.findBooksByAuthor(author);
+        return new ResponseEntity<>(books,HttpStatus.CREATED);
+    }
+    @GetMapping("get-books-by-genre")
+    public ResponseEntity<List<Book>> getBooksByGenre(@RequestParam String genre){
+        List<Book> books = bookService.findBooksByAuthor(genre);
+        return new ResponseEntity<>(books,HttpStatus.CREATED);
+    }
+    @DeleteMapping("delete-book-by-id/{id}")
+    public ResponseEntity<String> deleteBookById(@PathVariable int id){
+        bookService.deleteBookById(String.valueOf(id));
+        return new ResponseEntity<>("success",HttpStatus.OK);
+    }
+    @DeleteMapping("delete-all-books")
+    public ResponseEntity<String> deleteAllBooks(){
+        bookService.deleteAllBooks();
+        return new ResponseEntity<>("success",HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/driver/BookRepository.java
+++ src/main/java/com/driver/BookRepository.java
@@ -9,33 +9,53 @@ public class BookRepository {
     public BookRepository(){
         
     }
-
+    HashMap<Integer,Book> booksDb = new HashMap<>();
     public Book save(Book book){
-        return null;
+        booksDb.put(book.getId(),book);
+        return book;
     }
 
     public Book findBookById(int id){
 
-        return null;
+        return booksDb.get(id);
     }
 
     public List<Book> findAll(){
-        return null;
+        List<Book> books = new ArrayList<>();
+        for(Book book :booksDb.values())
+            books.add(book);
+        return books;
     }
 
     public void deleteBookById(int id){
+        booksDb.remove(id);
         return;
     }
 
     public void deleteAll(){
+        booksDb.clear();
         return;
     }
 
     public List<Book> findBooksByAuthor(String author){
-        return null;
+        List<Book> books = new ArrayList<>();
+        for(Book book :booksDb.values())
+        {
+            if(book.getAuthor().equals(author)){
+                books.add(book);
+            }
+        }
+        return books;
     }
 
     public List<Book> findBooksByGenre(String genre){
-        return null;
+        List<Book> books = new ArrayList<>();
+        for(Book book :booksDb.values())
+        {
+            if(book.getGenre().equals(genre)){
+                books.add(book);
+            }
+        }
+        return books;
     }
 }
